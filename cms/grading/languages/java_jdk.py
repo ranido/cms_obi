e3b8cc471a93956e65e836d67c3f599b5ddf10f5
ranido <ranido@gmail.com>
2021-09-24 21:58:35 -0300

    increase java mem+stack

--- cms/grading/languages/java_jdk.py
+++ cms/grading/languages/java_jdk.py
@@ -90,10 +90,10 @@ class JavaJDK(Language):
         if JavaJDK.USE_JAR:
             # executable_filename is a jar file, main is the name of
             # the main java class
-            return [["/usr/bin/java", "-Deval=true", "-Xmx512M", "-Xss64M",
+            return [["/usr/bin/java", "-Deval=true", "-Xmx1024M", "-Xss512M",
                      "-cp", executable_filename, main] + args]
         else:
             unzip_command = ["/usr/bin/unzip", executable_filename]
-            command = ["/usr/bin/java", "-Deval=true", "-Xmx512M", "-Xss64M",
+            command = ["/usr/bin/java", "-Deval=true", "-Xmx1024M", "-Xss512M",
                        main] + args
             return [unzip_command, command]
