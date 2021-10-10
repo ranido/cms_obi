8dc77b4f2a0314bec73223375503b1b5a72a2db8
Ricardo Anido <ranido@gmail.com>
2021-10-10 13:30:50 +0000

    add swapoff -a to isolate-set-environment.sh

--- isolate-set-environment.sh
+++ isolate-set-environment.sh
@@ -2,3 +2,4 @@ echo 0 > /proc/sys/kernel/randomize_va_space
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 echo never > /sys/kernel/mm/transparent_hugepage/defrag
 echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
+swapoff -a
